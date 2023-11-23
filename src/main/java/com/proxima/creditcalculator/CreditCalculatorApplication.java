071c2b9281272cda6d5cafe4a0d135dc6c4672ea
Taiwo Maja <taiwom@klluster.com>
2023-11-23 20:53:09 +0200

    initialize project

---
+++ src/main/java/com/proxima/creditcalculator/CreditCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.proxima.creditcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proxima/creditcalculator/CreditCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proxima.creditcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
